481b74a56125b9529936e6bffaa68e200afab373
Guillaume Lebigot <axel@teri-chan.net>
2019-08-22 10:26:47 +0200

    Added CHANGELOG and proper type definitions

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# CHANGELOG
+
+## 1.0.2
+
+- Added proper type definitions when used as a module
+
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,10 @@
-export interface ToyundaData {
-	lyrics: string[],
-	frames: string[]
+declare module 'toyunda2ass' {
+
+	export interface ToyundaData {
+		lyrics: string[],
+		frames: string[]
+	}
+	export function splitTime(time: string): ToyundaData
+	export function findFPS(videofile: string): Promise<number>
+	export function convertToASS(ToyundaData: ToyundaData, fps: number): string
 }
